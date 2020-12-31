aa0253b3ee8374a5f14fddb7111eb038b6b584ff
Emad A. Ismael <EmadAIsmael@yahoo.com>
2020-12-31 23:44:13 +0200

    Dec 31 2020 23:44 commit

--- Smart Calculator (2)/task/src/calculator/Main.kt
+++ Smart Calculator (2)/task/src/calculator/Main.kt
@@ -4,7 +4,6 @@ import kotlin.math.pow
 import kotlin.system.exitProcess
 
 val vars = mutableMapOf<String, Int?>()
-//val cmd = "(/help)|(/exit)".toRegex()
 
 fun main() {
 
@@ -20,22 +19,33 @@ fun main() {
                     }
                 }
                 isAssignment(input) -> {
-                    if (hasMultipleAssignments(input)) println("Invalid assignment")
-                    if (hasInvalidLvalue(input)) println("Invalid identifier")
-                    if (hasInvalidRvalue(input)) {
-                        if (isValidIdentifier(getRValue(input)) == true &&
-                            !isValidVar(getRValue(input))
-                        ) {
-                            println("Unknown variable")
-                        } else
-                            println("Invalid assignment")
+                    try {
+                        doAssignment(input)
+                    } catch (e: InvalidAssignmentException) {
+                        println(e.message)
+                    } catch (e: InvalidIdentifierException) {
+                        println(e.message)
+                    } catch (e: UnknownVariableException) {
+                        println(e.message)
+                    } catch (e: NumberFormatException) {
+                        println(e.message)
                     }
-                    if (isValidAssignment(input)) doAssignment(input)
+                }
+                isValidOneTerm(input) -> {
+                    try {
+                        println(tokenToVal(input))
+                    } catch (e: UnknownVariableException) {
+                        println(e.message)
+                    }
+
                 }
                 isExpression(input) -> {
-                    val output = evaluate(input)
-                    if (output != Unit)
+                    try {
+                        val output = evaluate(input)
                         println(output)
+                    } catch (e: InvalidExpressionException) {
+                        println(e.message)
+                    }
                 }
                 else -> println("Invalid expression")
             }
@@ -44,160 +54,147 @@ fun main() {
 }
 
 fun isValidOneTerm(inputString: String): Boolean {
-    val validOneValue = """^[-+]?\d+$""".toRegex()
-    val match = validOneValue.find(inputString)
-    return match?.value?.isNotEmpty() == true
+    val validOneValue = """^[-+]?\s*\d+|[a-zA-Z]+$""".toRegex()
+    return inputString.matches(validOneValue)
 }
 
-//fun hasEndingOperator(inputString: String): Boolean {
-//    return """[-+]$""".toRegex().find(inputString)?.value?.isNotEmpty() == true
-//}
+fun evaluate(inputString: String): Int? {
+    // handle multiple +'s and / or -'s
+    val oPlus = "\\+{2,}".toRegex()
+    val oMinus = "-{2,}".toRegex()
+    var tokensString = oPlus.replace(inputString, "+")
+    tokensString = oMinus.replace(tokensString) { m ->
+        if (m.value.length % 2 == 0) "+" else "-"
+    }
 
-//fun hasNoOperator(inputString: String): Boolean {
-//    return """\d+\s+\d+""".toRegex().find(inputString)?.value?.isNotEmpty() == true
-//}
+    // handle multiple *'s, ^'s and / or /'s
+    if ("""[/^*]{2,}""".toRegex().containsMatchIn(tokensString))
+        throw InvalidExpressionException()
 
-//fun isValidArithmetic(inputString: String): Boolean {
-//
-//    val operator = """[-+]+"""
-//    val operand = """\d+"""
-//    val sign = """[-+]?"""
-//    val term = """$sign?${operand}"""
-//    val validExp = """${term}(\s*${operator}\s*${term})*"""
-//
-//    val match = validExp.toRegex().find(inputString)
-//    return (!hasEndingOperator(inputString)) &&
-//            !hasNoOperator(inputString) &&
-//            (match?.value?.isNotEmpty() == true)
-//}
+    val postfix = toPostfix(tokensString)       // param was inputString
 
-//fun evaluate(inputString: String): Any {
-//    val input = inputString.split("\\s+".toRegex()).toMutableList()
-//    val ops = mapOf(
-//        "+" to { a: Int, b: Int -> a + b },
-//        "-" to { a: Int, b: Int -> a - b },
-//        "*" to { a: Int, b: Int -> a * b },
-//        "/" to { a: Int, b: Int -> a / b },
-//        "^" to { a: Int, b: Int -> Math.pow(a.toDouble(), b.toDouble()).toInt() }
-//    )
-//    var op = ""
-//    // step through input values
-//    // to check operator
-//    for (i in 0..input.lastIndex - 2 step 2) {
-//        // if operator is "+"
-//        if (input[i + 1].all { it == '+' })
-//            op = "+"
-//        // if operator is "-", odd number of "-"'s is "-", else "+".
-//        else if (input[i + 1].all { it == '-' })
-//            op = if (input[i + 1].length % 2 != 0) "-" else "+"
-//
-//        val a = tokenToVal(input[i])
-//        val b = tokenToVal(input[i + 2])
-//        input[i + 2] = ops[op]?.invoke(a, b).toString()
-//    }
-//    return try {
-//        val output = input[input.lastIndex].toInt()
-//        output
-//    } catch (e: NumberFormatException) {
-//        val v = input[input.lastIndex]
-//        if (isValidVar(v)) tokenToVal(v)
-//        else println("Unknown variable")
-//    }
-//}
+    if (postfix.contains("("))
+        throw InvalidExpressionException()
 
-fun evaluate(inputString: String): Any {
-    val input = inputString.split("\\s+".toRegex()).toMutableList()
-    val ops = mapOf(
-        "+" to { a: Int, b: Int -> a + b },
-        "-" to { a: Int, b: Int -> a - b },
-        "*" to { a: Int, b: Int -> a * b },
-        "/" to { a: Int, b: Int -> a / b },
-        "^" to { a: Int, b: Int -> a.toDouble().pow(b.toDouble()).toInt() }
-    )
-    val postfix = toPostfix(inputString)
-
-//    var op = ""
-//    // step through input values
-//    // to check operator
-//    for (i in 0..input.lastIndex - 2 step 2) {
-//        // if operator is "+"
-//        if (input[i + 1].all { it == '+' })
-//            op = "+"
-//        // if operator is "-", odd number of "-"'s is "-", else "+".
-//        else if (input[i + 1].all { it == '-' })
-//            op = if (input[i + 1].length % 2 != 0) "-" else "+"
-//
-//        val a = tokenToVal(input[i])
-//        val b = tokenToVal(input[i + 2])
-//        input[i + 2] = ops[op]?.invoke(a, b).toString()
-//    }
-//    return try {
-//        val output = input[input.lastIndex].toInt()
-//        output
-//    } catch (e: NumberFormatException) {
-//        val v = input[input.lastIndex]
-//        if (isValidVar(v)) tokenToVal(v)
-//        else println("Unknown variable")
-//    }
-
-    return 0
+    return evaluatePostfix(postfix)
 }
 
-fun toPostfix(input: String) {
+fun toPostfix(input: String): String {
     val opStack = Stack()
     var result = ""
 
-    // handle multiple +'s and / or -'s
-    val oPlus = "\\+{2,}".toRegex()
-    val oMinus = "\\-{2,}".toRegex()
-    var tokensString = oPlus.replace(input, "+")
-    tokensString = oMinus.replace(input) { m ->
-        if (m.value.length % 2 == 0) "+" else "-"
-    }
+    var tokensString = input
     tokensString.replace(" ", "")
 
     // delimit tokens with spaces
     val opRegex = """[-+*/^()]""".toRegex()
-    tokensString = opRegex.replace(input) { m -> "${m.value} " }
+    tokensString = opRegex.replace(tokensString) { m -> " ${m.value} " }
 
     // tokenize
-    val tokens = tokensString.split("\\s".toRegex()).toMutableList()
+    val tokens = tokensString
+        .split("\\s".toRegex()).toMutableList()
         .filter { it.isNotEmpty() }
-    println(tokens)
+
+    // process expression
     for (token in tokens) {
+        // number or variable, add to result
         if (isValidOneTerm(token) || isValidVar(token))
-            result += tokenToVal(token).toString()
-        else if (opStack.isEmpty() || opStack.peek() == "(")
-            opStack.push(token)
-        else if (Operator.findBySymbol(token).precedence >
-            Operator.findBySymbol(opStack.peek().toString()).precedence
-        )
-            opStack.push(token)
-        else if (Operator.findBySymbol(token).precedence <=
-            Operator.findBySymbol(opStack.peek().toString()).precedence
-        ) {
-            while ((Operator.findBySymbol(token).precedence <=
-                        Operator.findBySymbol(opStack.peek().toString()).precedence) ||
-                opStack.peek() != "("
-            ) {
-                result += opStack.pop()
+            try {
+                result += tokenToVal(token).toString() + " "
+            } catch (e: UnknownVariableException) {
+                println(e.message)
             }
-            if (opStack.peek() == "(") opStack.pop()
-        } else if (token == "(") opStack.push(token)
+
+        // stack is empty or stack top == "(", push operator to stack
+        else if (isOperator(token)) {
+            if (opStack.isEmpty() || opStack.peek() == "(")
+                opStack.push(token)
+            // operator.precedence > stack.top.precedence
+            else if (Operator.findBySymbol(token).precedence >
+                Operator.findBySymbol(opStack.peek().toString()).precedence
+            )
+                opStack.push(token)
+            // incoming operator has lower or equal precedence compared to the top of the stack
+            else if (Operator.findBySymbol(token).precedence <=
+                Operator.findBySymbol(opStack.peek().toString()).precedence
+            ) {
+                while (opStack.isNotEmpty() && (
+                            (Operator.findBySymbol(token).precedence <=
+                                    Operator.findBySymbol(opStack.peek().toString()).precedence) &&
+                                    opStack.peek() != "(")
+                ) {
+                    result += opStack.pop() + " "
+                }
+                // if (opStack.peek() == "(") opStack.pop()
+                opStack.push(token)
+            }
+        }
+        // incoming element is a left parenthesis, push it on the stack
+        else if (token == "(") opStack.push(token)
+        // incoming element is a right parenthesis,
+        // pop the stack and add operators to the result until you see
+        // a left parenthesis. Discard the pair of parentheses
         else if (token == ")") {
-            while (opStack.peek() != "(")
-                result += opStack.pop()
-            opStack.pop()               // discard "("
+            while (opStack.isNotEmpty() && opStack.peek() != "(")
+                result += opStack.pop() + " "
+            if (opStack.peek() == "(") opStack.pop()               // discard "("
+            else
+                throw InvalidExpressionException()
         }
     }
-    while (!opStack.isEmpty())
-        result += opStack.pop()
+    // expression is empty, pop all operator and add to result
+    while (opStack.isNotEmpty())
+        result += opStack.pop() + " "
 
-    println(result)
+    return result
 }
 
+fun evaluatePostfix(postfix: String): Int? {
+    val calcStack = Stack()
+    val pfTokens = postfix.split(" ").filter { it.isNotEmpty() }
+
+    for (token in pfTokens) {
+        if (isValidOneTerm(token) || isValidVar(token))
+            try {
+                calcStack.push(tokenToVal(token).toString())
+            } catch (e: UnknownVariableException) {
+                println(e.message)
+            }
+
+        else if (isOperator(token)) {
+            val opObj = Operator.findBySymbol(token)
+            val a = calcStack.pop()?.toInt()
+            val b = calcStack.pop()?.toInt()
+
+            var result = 0
+            if (a != null && b != null)
+                result = opObj.method.invoke(b, a)
+            calcStack.push(result.toString())
+        }
+    }
+    return calcStack.pop()?.toInt()
+}
+
+fun isOperator(op: String): Boolean = "+-/*^".contains(op)
+
 fun tokenToVal(token: String): Int {
-    return if (isValidVar(token)) vars[token]!! else token.toInt()
+//    var newToken = ""
+    if (token.startsWith("-")) {
+        val newToken = token.substring(token.indexOf('-') + 1)
+        return if (isValidVar(newToken)) -1 * vars[newToken]!! else -1 * newToken.toInt()
+    }
+//    return if (isValidVar(token)) vars[token] else token.toInt()
+//    return if (isValidVar(token))
+//        vars[token]!!
+//    else if (isValidIdentifier(token))
+//        throw UnknownVariableException()
+//    else
+//        token.toInt()
+    return when {
+        isValidVar(token) -> vars[token]!!
+        isValidIdentifier(token) -> throw UnknownVariableException()
+        else -> token.toInt()
+    }
 }
 
 fun isAssignment(input: String): Boolean {
@@ -234,30 +231,37 @@ fun doCommand(cmd: String) {
     }
 }
 
-fun isValidIdentifier(varName: String?): Boolean? {
-    return varName?.matches("""[a-zA-Z]+""".toRegex())
+fun isValidIdentifier(varName: String): Boolean {
+    return varName.matches("""[a-zA-Z]+""".toRegex())
 }
 
 fun isValidVar(identifier: String): Boolean {
     return vars.containsKey(identifier)
 }
 
-fun isValidAssignment(assignment: String): Boolean {
-    return if (assignment.split("=").size == 2) {
-        val (lValue, rValue) = assignment.split("=").map { it.trim() }
-        isValidIdentifier(lValue) == true &&
-                (isValidVar(rValue) || isValidOneTerm(rValue))
-    } else
-        false
+fun doAssignment(input: String) {
+    val (identifier, rValue) = input.split("=").map { it.trim() }
+
+    if (hasMultipleAssignments(input))
+        throw InvalidAssignmentException()
+
+    if (hasInvalidLvalue(input))
+        throw InvalidIdentifierException()
+
+    if (!isValidIdentifier(rValue) && !isAllNumeric(rValue))
+        throw InvalidAssignmentException()
+
+    if (!isValidVar(rValue) && !isAllNumeric(rValue))
+        throw UnknownVariableException()
+
+    if (isValidVar(rValue))
+        vars[identifier] = vars[rValue]
+    else
+        vars[identifier] = rValue.toInt()
 }
 
-fun doAssignment(input: String) {
-    val (identifier, mapping) = input.split("=").map { it.trim() }
-
-    if (isValidIdentifier(mapping) == true && isValidVar(mapping))
-        vars[identifier] = vars[mapping]
-    else
-        vars[identifier] = mapping.toInt()
+fun isAllNumeric(input: String): Boolean {
+    return input.matches("""^[-+]?\s*\d+$""".toRegex())
 }
 
 fun hasMultipleAssignments(input: String): Boolean {
@@ -267,35 +271,23 @@ fun hasMultipleAssignments(input: String): Boolean {
 fun hasInvalidLvalue(input: String): Boolean {
     val possibleAssignment = """=""".toRegex().findAll(input).count() == 1
     val lValue = input.split("=")[0].trim()
-    return possibleAssignment && !isValidIdentifier(lValue)!!
+    return possibleAssignment && !isValidIdentifier(lValue)
 }
 
-fun hasInvalidRvalue(input: String): Boolean {
-    val possibleAssignment = """=""".toRegex().findAll(input).count() == 1
-    return if (possibleAssignment) {
-
-        val rValue = getRValue(input)
-        possibleAssignment &&
-                !(isValidVar(rValue) ||
-                        isValidOneTerm(rValue))
-    } else
-        false
-}
-
-fun getRValue(input: String): String {
-    return input.split("=")[1].trim()
-}
-
-class Stack() {
+class Stack {
     private val store = mutableListOf<String>()
 
-    fun push(item: String) {
-        store.add(item)
+    fun push(item: String?) {
+        store.add(item.toString())
     }
 
-    fun pop(): String {
-        val i = store.last()
-        return i
+    fun pop(): String? {
+        return if (store.isNotEmpty()) {
+            val i = store.last()
+            store.removeAt(store.lastIndex)
+            i
+        } else
+            null
     }
 
     fun peek(): String? {
@@ -305,19 +297,52 @@ class Stack() {
     fun isEmpty(): Boolean {
         return store.size == 0
     }
+
+    fun isNotEmpty(): Boolean = !this.isEmpty()
 }
 
-enum class Operator(val symbol: String, val precedence: Int) {
-    PLUS("+", 1),
-    MINUS("-", 1),
-    MULTIPLY("*", 2),
-    DIVIDE("/", 2),
-    POWER("^", 3),
-    NULL("", 0);
+class Queue {
+    private val s1 = Stack()
+    private val s2 = Stack()
+
+    fun enqueue(e: String) {
+        s1.push(e)
+    }
+
+    fun dequeue(): String? {
+        if (s2.isEmpty())
+            if (s1.isEmpty())
+                throw EmptyQueueException()
+
+        while (s1.isNotEmpty())
+            s2.push(s1.pop())
+        return s2.pop()
+    }
+
+    fun isEmpty(): Boolean {
+        return s1.isEmpty() && s2.isEmpty()
+    }
+
+    fun isNotEmpty(): Boolean {
+        return !this.isEmpty()
+    }
+}
+
+enum class Operator(
+    val symbol: String,
+    val precedence: Int,
+    val method: (Int, Int) -> Int
+) {
+    PLUS("+", 1, { a: Int, b: Int -> a + b }),
+    MINUS("-", 1, { a: Int, b: Int -> a - b }),
+    MULTIPLY("*", 2, { a: Int, b: Int -> a * b }),
+    DIVIDE("/", 2, { a: Int, b: Int -> a / b }),
+    POWER("^", 3, { a: Int, b: Int -> a.toDouble().pow(b.toDouble()).toInt() }),
+    NULL("", 10, { a: Int, _: Int -> a });
 
     companion object {
         fun findBySymbol(symbol: String): Operator {
-            for (enum in Operator.values()) {
+            for (enum in values()) {
                 if (symbol == enum.symbol)
                     return enum
             }
@@ -327,3 +352,8 @@ enum class Operator(val symbol: String, val precedence: Int) {
 }
 
 class UnknownCommandException(message: String = "Unknown command") : Exception(message)
+class InvalidExpressionException(message: String = "Invalid expression") : Exception(message)
+class EmptyQueueException(message: String = "Empty queue exception") : Exception(message)
+class UnknownVariableException(message: String = "Unknown variable") : Exception(message)
+class InvalidAssignmentException(message: String = "Invalid assignment") : Exception(message)
+class InvalidIdentifierException(message: String = "Invalid identifier") : Exception(message)
